450a2959159057b729584699cdcee14c74b7a1df
sanjidarimi023 <sanjidarimi023@gmail.com>
2025-12-20 23:38:07 +0600

    added store.js file for state handle using zustand

--- src/pages/QuestionBuilder/question-builder.tsx
+++ src/pages/QuestionBuilder/question-builder.tsx
@@ -1,10 +1,19 @@
-
 const QuestionBuilder = () => {
-    return (
-        <div>
-            Question builder section
-        </div>
-    );
+  return (
+    <section>
+      <div className="flex justify-center items-center text-center flex-col my-10 space-y-4">
+        <h2 className="text-5xl font-bold">Build your{" "} 
+            <span className="text-purple-700">Question Paper</span>
+        </h2>
+        <p className="text-lg font-medium">
+          Build section-wise question papers, preview changes in real time, and export when ready.
+        </p>
+      </div>
+      <div>
+
+      </div>
+    </section>
+  );
 };
 
 export default QuestionBuilder;
